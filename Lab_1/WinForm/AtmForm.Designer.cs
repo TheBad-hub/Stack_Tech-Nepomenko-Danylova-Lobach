d202bcd40d3d6f635deedc2e3a726403f8da3d12
DaniilLB <lobach.daniel.2005@gmail.com>
2024-09-25 00:14:35 +0300

    1000-7

--- Lab_1/WinForm/AtmForm.Designer.cs
+++ Lab_1/WinForm/AtmForm.Designer.cs
@@ -27,7 +27,7 @@ namespace WinForm
 
         /// <summary>
         /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        /// the contents of this method with the code editor./
         /// </summary>
         private void InitializeComponent()
         {
