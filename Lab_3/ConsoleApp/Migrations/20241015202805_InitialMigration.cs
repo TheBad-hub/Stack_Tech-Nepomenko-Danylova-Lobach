a6cf5e032ebb6228022696f50d8226ccf9e387f9
The Bad <149719926+TheBad-hub@users.noreply.github.com>
2024-10-15 23:28:22 +0300

    added migration

---
+++ Lab_3/ConsoleApp/Migrations/20241015202805_InitialMigration.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConsoleApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Policemen",
+                columns: table => new
+                {
+                    PolicemanId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BadgeNumber = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Policemen", x => x.PolicemanId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Offenders",
+                columns: table => new
+                {
+                    OffenderId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ViolationType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PolicemanId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Offenders", x => x.OffenderId);
+                    table.ForeignKey(
+                        name: "FK_Offenders_Policemen_PolicemanId",
+                        column: x => x.PolicemanId,
+                        principalTable: "Policemen",
+                        principalColumn: "PolicemanId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Offenders_PolicemanId",
+                table: "Offenders",
+                column: "PolicemanId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Offenders");
+
+            migrationBuilder.DropTable(
+                name: "Policemen");
+        }
+    }
+}
